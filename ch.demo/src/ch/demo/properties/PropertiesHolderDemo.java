45da432a78e8449c2c353626fb58452810763e25
ckulesza <ckulesza@hsz-t.ch>
2011-10-13 15:23:18 +0200

    Signed-off-by: ckulesza <ckulesza@hsz-t.ch>

--- ch.demo/src/ch/demo/properties/PropertiesHolderDemo.java
+++ ch.demo/src/ch/demo/properties/PropertiesHolderDemo.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 public class PropertiesHolderDemo {
 
 	public static void main(String[] args) {
-		PropertiesHolder.setFilePath("");
+//		PropertiesHolder.setFilePath("");
 		PropertiesHolder.setFileName("config.properties");
 		try {
 			PropertiesHolder.loadPropertiesFile();
 			System.out.println(PropertiesHolder.getProperties().getProperty("test"));
-			PropertiesHolder.getProperties().put("test", "TestProperty");
+			PropertiesHolder.getProperties().put("test2", "TestProperty");
 			PropertiesHolder.saveProperiesFile();
 			PropertiesHolder.loadPropertiesFile();
-			System.out.println(PropertiesHolder.getProperties().getProperty("test"));
+			System.out.println(PropertiesHolder.getProperties().getProperty("test2"));
 		} catch (NullPointerException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
